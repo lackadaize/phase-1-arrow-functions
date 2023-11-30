b9338cf008a7cba12c272f26057266b6c1a3a173
Jonathan Kulak <lackadaize@msn.com>
2023-11-30 09:24:32 -0500

    This is the git commit for the Arrow Functions assignment.

---
+++ index.js
@@ -0,0 +1,11 @@
+// divide function
+const divide = (num1, num2) => num1 / num2;
+console.log(divide(2000, 100));
+
+// square function
+const square = (x) => x * 2;
+console.log(square(2));
+
+// add function
+const add = (a, b) => a + b;
+console.log(add(3, 4));
